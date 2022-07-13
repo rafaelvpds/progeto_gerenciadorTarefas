5197fb643ebb1ff5edb8f7b5eaa7285b521c3300
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2022-07-13 17:25:08 -0300

    index, js e CSS
    Atribuição de comentários para melhor entendimento do projeto. a inclusão do loading inicial

--- README.md
+++
@@ -1,2 +0,0 @@
-# progeto_gerenciadorTarefas
- Projeto Final Modulo 1 Arnia
--- acess/js/manager.js
+++ acess/js/manager.js
@@ -1,44 +1,72 @@
 
-var bancoTask = [];
 var addTask = document.getElementById("btnTask");
 var modal = document.getElementById("modalTask");
 var cancModal = document.getElementById("btnCancelar");
 var salvarModal = document.getElementById("btnSalvar");
+
+
+// Dispara a ação para abrir o modal
 addTask.onclick = function () {
     modal.style.display = "block";
 }
-
+// Dispara a ação no botão Cancelar para fechar o modal sem trazer nenhum resultado
 cancModal.onclick = function () {
     modal.style.display = "none";
 }
 
+
+// Função de Carregamento de Tela
+
+function loadingScreen() {
+    window.setTimeout(() => {
+        document.getElementById("loadingTasck").style.display = "none"
+    }, 5000)
+}
+
+window.addEventListener("load", (event) => {
+    loadingScreen();
+})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("loadingTasck").style.display = "block";
+})
+
+
+
+// Criei uma Variavel para iniciar as tarefas a apartir do numero 0
 var i = 0
 salvarModal.onclick = function () {
-    var status
-    i = i+1;
-   
+
+
+
+    i = i + 1;
+    // o input NUMERO recebe o valor de i que inicialmelte e 0
     var numeroTask = document.getElementById("numberTask").value;
     document.getElementById("numberTask").value = i
-    
+    // recebe o valor da DESCRIÇÃO da tarefa que vai ser realizada
     var descricaoTask = document.getElementById("descriptionTask").value;
+    // recebe o valor da DATA que vai entregue a tarefa
     var dataTarefa = document.getElementById("dateTask").value;
 
     // Capturando o Select
     var select = document.querySelector('#options');
-    // var statusTask = select.options[select.selectedIndex].text;
-    var valueTask =select.value;
- 
-    if(valueTask === "Concluida"){
-        status= "green"
-    }else if(valueTask === "Em Andamento"){
-        status ="yellow"
-    }else if(valueTask ==="Parado"){
+    var valueTask = select.value;
+    // Fazendo uma condição para validar qual condição STATUS do select
+    //Criei uma variavel "status" para receber o valor ou a Cor na qual vai ser passada quando estivermos jogando 
+    //o  resultado da condição
+    var status
+    if (valueTask === "Concluida") {
+        status = "green"
+    } else if (valueTask === "Em Andamento") {
+        status = "yellow"
+    } else if (valueTask === "Parado") {
         status = "red"
     }
-    
+
+    //ESTOU PASSANDO OS VALORE RECEBIDOS PELO USUARIO E IMPRIMINDO NA TABELA
 
     document.getElementById('tbody').innerHTML = document.getElementById('tbody').innerHTML +
-     `
+        `
            <tr>
                 <td>${numeroTask} </td>
                 <td>${descricaoTask} </td>
@@ -50,11 +78,11 @@ salvarModal.onclick = function () {
            </tr>
 
      `
-     modal.style.display = 'none';
-    }
+    modal.style.display = 'none';
+}
 
- 
- 
+
+
 
 
 
