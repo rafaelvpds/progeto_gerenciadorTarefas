95f6c038ffbcd940523f313c9c758d47171d58a9
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2022-07-14 20:04:35 -0300

    JS CSS index
    Criação do objeto Task
    Criação das funções:
    validateInput()
    printTask()
    msg com animação

--- acess/js/manager.js
+++ acess/js/manager.js
@@ -1,8 +1,112 @@
+// // Função de Carregamento de Tela
 
-var addTask = document.getElementById("btnTask");
-var modal = document.getElementById("modalTask");
-var cancModal = document.getElementById("btnCancelar");
-var salvarModal = document.getElementById("btnSalvar");
+function loadingScreen() {
+    window.setTimeout(() => {
+        document.getElementById("loadingTask").style.display = "none"
+    }, 1000)
+}
+
+window.addEventListener("load", (event) => {
+    loadingScreen();
+})
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("loadingTask").style.display = "block";
+})
+// ----------------------------------------------------------------------------
+
+let bancoTask =[];
+let addTask = document.getElementById("btnTask");
+let modal = document.getElementById("modalTask");
+let cancModal = document.getElementById("btnCancelar");
+
+let salvarModal = document.getElementById("btnSalvar");
+
+
+
+
+salvarModal.onclick = () =>{
+
+let inptNumberTask = document.getElementById("numberTask").value;
+let inptDescriptionTask = document.getElementById("descriptionTask").value;
+let inptDateTask = document.getElementById("dateTask").value;
+let select = document.querySelector('#options');
+let inptstatusTask = select.value;
+
+
+validateInput(inptDescriptionTask, document.getElementById("descricao-obrigatorio").innerHTML ="Preencha o Campo Descrção")
+validateInput(inptDateTask, document.getElementById("campo-data").innerHTML ="Preencha Data")
+
+validateInput(inptstatusTask, document.getElementById("status-obrigatotio").innerHTML ="Preencha o Campo Status")
+
+
+if(inptNumberTask !=='' && inptDescriptionTask !==''&& inptDateTask!==''&inptstatusTask!==''){
+
+    let task ={
+            numberTask: inptNumberTask,
+            descriptionTask: inptDescriptionTask,
+            dateTask: inptDateTask,
+            statusTask: inptstatusTask,  
+        }
+        bancoTask.push(task)
+        // console.log(task)
+        printTask()
+
+    }
+}
+
+let printTask = ()=>{
+
+    let lista =''
+    console.log(bancoTask)
+    bancoTask.forEach((task)=>{
+
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "green"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "yellow"
+        } else if (task.statusTask === "Parado") {
+            status = "red"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="../acess/imagem/icones/editar.png">
+                            <img src="../acess/imagem/icones/excluir.png">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+    })
+    document.getElementById('tbody').innerHTML = lista
+
+    // console.log("fora",lista)
+}
+let validateInput = (valorInput, msgObrigatorio) =>{
+ 
+    if(valorInput===''){
+        // console.log("vazio")
+        msgObrigatorio = ""
+        console.log(msgObrigatorio)
+      
+    }else{
+        msgObrigatorio= ""
+    }
+}
+
+
+
+
 
 
 // Dispara a ação para abrir o modal
@@ -14,90 +118,46 @@ cancModal.onclick = function () {
     modal.style.display = "none";
 }
 
-
-// Função de Carregamento de Tela
-
-function loadingScreen() {
-    window.setTimeout(() => {
-        document.getElementById("loadingTask").style.display = "none"
-    }, 5000)
+// Dispara a ação para abrir o modal
+addTask.onclick = function () {
+    modal.style.display = "block";
+}
+// Dispara a ação no botão Cancelar para fechar o modal sem trazer nenhum resultado
+cancModal.onclick = function () {
+    modal.style.display = "none";
 }
 
-window.addEventListener("load", (event) => {
-    loadingScreen();
-})
+//     function cadastradoComSucesso() {
+//         let sucesso = document.getElementById("alerta");
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById("loadingTask").style.display = "block";
-})
+//         sucesso.innerHTML = 'Cadastrado com sucesso.';
 
-
-
-// Criei uma Variavel para iniciar as tarefas a apartir do numero 0
-var i = 0
-salvarModal.onclick = function () {
+//         sucesso.classList.add("alert-success", "animate__fadeInUp"); //CSS do bootstrap
+//         sucesso.classList.remove("d-none");
 
 
+//         window.setTimeout(() => {
 
-    i = i + 1;
-    // o input NUMERO recebe o valor de i que inicialmelte e 0
-    var numeroTask = document.getElementById("numberTask").value;
-    document.getElementById("numberTask").value = i
-    // recebe o valor da DESCRIÇÃO da tarefa que vai ser realizada
-    var descricaoTask = document.getElementById("descriptionTask").value;
-    // recebe o valor da DATA que vai entregue a tarefa
-    var dataTarefa = document.getElementById("dateTask").value;
+//             sucesso.document.getElementById("alerta").classList.remove("animate__fadeInUp");
+//             // sucesso.document.getElementById("alerta").classList.add("animate__fadeOutDown")
 
-    // Capturando o Select
-    var select = document.querySelector('#options');
-    var valueTask = select.value;
+//         },
 
+//             2000);
 
-    if (descricaoTask === "") {
-        alert("decrição vazia")
-        document.getElementById("descricao-obrigatorio").innerHTML = "Campo descrição Obrigatorio";
-    } else {
-        document.getElementById("descricao-obrigatorio").innerHTML = "";
-    }
-    if (valueTask === "") {
-        document.getElementById("status-obrigatotio").innerHTML = "Escolha um Status"
-    } else {
-        document.getElementById("status-obrigatotio").innerHTML = "";
-    }
-    if (dataTarefa === null) {
-        console.log(dataTarefa)
-        document.getElementById("campo-data").innerHTML = "Data de entrega Obrigatorio"
-    }
+//         window.setTimeout(() => {
+            
+//             sucesso.classList.add("animate__fadeOutDown");
+//             sucesso.classList.remove("d-none");
+//         },
 
-    if (descricaoTask !== "" && valueTask !== "") {
-        var status
-        if (valueTask === "Concluida") {
-            status = "green"
-        } else if (valueTask === "Em Andamento") {
-            status = "yellow"
-        } else if (valueTask === "Parado") {
-            status = "red"
-        }
+//             1000);
 
-        document.getElementById('tbody').innerHTML = document.getElementById('tbody').innerHTML +
-            `
-           <tr>
-                <td>${numeroTask} </td>
-                <td>${descricaoTask} </td>
-                <td>${dataTarefa} </td>
-                <td class="${status}">${valueTask} </td>
-                
-                <td><img src="../acess/imagem/icones/editar.png">
-                <img src="../acess/imagem/icones/excluir.png">  </td>
-           </tr>
-
-     `
-        modal.style.display = 'none';
-    }
+//         window.setTimeout(() => {
+//             sucesso.classList.add("d-none");
+//             sucesso.classList.remove("animate__fadeOut");
+//         })
+//     }
 
 
-
-    //ESTOU PASSANDO OS VALORE RECEBIDOS PELO USUARIO E IMPRIMINDO NA TABELA
-
-
-}
+// }
