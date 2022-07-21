e79288b80d759ba967ab0766aedd9ec108855828
rafaelvpds <33781893+rafaelvpds@users.noreply.github.com>
2022-07-21 14:27:49 -0300

    js CSS
    Ordenação por:
    Numero, Data e Status e Descrição
    
    Plano de fundo Barra Amarela

--- acess/js/manager.js
+++ acess/js/manager.js
@@ -18,17 +18,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
   
 })
 
-
-
-
 // ----------------------------------------------------------------------------
 let addTask = document.getElementById("btnTask");
 let modal = document.getElementById("modalTask");
 let cancModal = document.getElementById("btnCancelar");
-
-
-
-
 
 async function adicionarTarefas(){
 
@@ -60,7 +53,7 @@ if(inptstatusTask===""){
 if(inptNumberTask !=='' && inptDescriptionTask !==''&& inptDateTask!==''&inptstatusTask!==''){
    
                 task ={
-                        numberTask: inptNumberTask,
+                        numberTask:  inptNumberTask,
                         descriptionTask: inptDescriptionTask,
                         dateTask: inptDateTask,
                         statusTask: inptstatusTask,  
@@ -98,8 +91,6 @@ let excluir = async(idTarefas) =>{
 
 }
 
-
-
 let editar = async (idTarefas) =>{
 
     modal.style.display = "block";
@@ -119,7 +110,8 @@ let editar = async (idTarefas) =>{
     salvarEd(idTarefas)} 
 }
 
-const salvarEd = async (idTarefas) =>{ // async function editarTask(){
+const salvarEd = async (idTarefas) =>{ 
+    // async function editarTask(){
     // let inputTarefa = document.getElementById("tarefa").value
     let inptNumberTask = document.getElementById("numberTask").value;
     let inptDescriptionTask = document.getElementById("descriptionTask").value;
@@ -150,7 +142,7 @@ const salvarEd = async (idTarefas) =>{ // async function editarTask(){
        
                     task ={
 
-                            numberTask: inptNumberTask,
+                            numberTask: parseInt(inptNumberTask) ,
                             descriptionTask: inptDescriptionTask,
                             dateTask: inptDateTask,
                             statusTask: inptstatusTask,  
@@ -172,11 +164,430 @@ const salvarEd = async (idTarefas) =>{ // async function editarTask(){
                     document.getElementById("dateTask").value = ""
 
                     document.getElementById("options").value = ""
-
+                    cadastradoComSucesso()
                     fecharModal()
          
         } 
 }
+
+function orderDescDecre(){
+    printOrderDescr()
+}
+function orderDescAsc(){
+    printOrderDescrAsc()
+}
+// ------------------------------------------------------------------------
+function orderDate(){
+    printOrderDate();
+}
+function orderDateAsc(){
+    printOrderDateAce();
+}
+// -----------------------------------------------------------------------
+function orderStatus(){
+    printOrderStatus()
+}
+function orderStatusAsc(){
+
+    printOrderAcenStatus()
+}
+// --------------------------------------------------------------------
+function orderNumDecre(){
+    printOrderNumDesc();
+
+    // document.getElementById('orderAsc').onclick = function(){ printOrderNumAsc();}
+   
+}
+function orderNumAsc(){
+    printOrderNumAsc()
+}
+// -------------------------------------------------------------------
+let buscarStatusDecres = async() =>{
+    let response = await fetch(
+        'http://localhost:3000/atividades/?_sort=statusTask&_order=desc') 
+    let descOrder = await response.json();  
+    console.log(descOrder)
+    return descOrder
+}
+let buscarStatusAsc = async()=>{
+    let response = await fetch(
+        ' http://localhost:3000/atividades/?_sort=statusTask&_order=asc') 
+    let statusOrdeAcs = await response.json();  
+
+    console.log(statusOrdeAcs)
+
+    return statusOrdeAcs
+}
+// --------------------------------------------------------------------
+let buscarDescDecres = async()=>{
+    let response = await fetch(
+        'http://localhost:3000/atividades/?_sort=dateTask&_order=desc') 
+    let descOrder = await response.json();  
+    console.log(descOrder)
+    return descOrder
+}
+
+let buscarDescAcen = async()=>{
+    let response = await fetch(
+        'http://localhost:3000/atividades/?_sort=dateTask&_order=asc') 
+    let descOrderAcen = await response.json();  
+    console.log(descOrderAcen)
+    return descOrderAcen
+}
+// ----------------------------------------------------------------------------
+let buscarDateDesc = async()=>{
+    let response = await fetch(
+        'http://localhost:3000/atividades/?_sort=dateTask&_order=desc') 
+    let dateOrder = await response.json();  
+    console.log(dateOrder)
+    return dateOrder
+}
+
+let buscarDateAc = async() =>{
+    let response = await fetch(
+        ' http://localhost:3000/atividades/?_sort=dateTaskk&_order=asc') 
+    let numOrderAsc = await response.json();  
+
+    console.log(numOrderAsc)
+
+    return numOrderAsc
+}
+// -----------------------------------------------------------------------------
+let buscarNumAsc = async() =>{
+    let response = await fetch(
+        ' http://localhost:3000/atividades/?_sort=numberTask&_order=asc') 
+    let numOrderAsc = await response.json();  
+
+    console.log(numOrderAsc)
+
+    return numOrderAsc
+}
+let buscarNumDesc = async() =>{
+    let response = await fetch(
+        'http://localhost:3000/atividades/?_sort=numberTask&_order=desc') 
+    let numOrder = await response.json();  
+    console.log(numOrder)
+    return numOrder
+}
+
+
+// --------------------------------------------------------------------
+
+let printOrderStatus = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarStatusDecres()
+
+    listTaskOrder.forEach((task) => {
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+let printOrderAcenStatus = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarStatusAsc()
+
+    listTaskOrder.forEach((task) => {
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+// --------------------------------------------------------------------------------------------
+
+let printOrderDescr = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarDescDecres()
+
+    listTaskOrder.forEach((task) => {
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+let printOrderDescrAsc = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarDescAcen()
+
+    listTaskOrder.forEach((task) => {
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+// ----------------------------------------------------------
+
+let printOrderDate = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarDateDesc()
+
+    listTaskOrder.forEach((task) => {
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+let printOrderDateAce = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarDateAc()
+
+    listTaskOrder.forEach((task) => {
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+
+// ---------------------------------
+let printOrderNumDesc = async() =>{
+    let lista = ""
+
+    const listTaskOrder = await buscarNumDesc()
+
+    listTaskOrder.forEach((task) => {
+        
+       
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+let printOrderNumAsc = async()=>{
+    let lista = ""
+
+    const listTaskOrder = await buscarNumAsc()
+
+    listTaskOrder.forEach((task) => {
+        
+       
+        let status
+
+        if (task.statusTask === "Concluida") {
+            status = "statusConcluid"
+        } else if (task.statusTask === "Em Andamento") {
+            status = "statusInProgress"
+        } else if (task.statusTask === "Parado") {
+            status = "statusStop"
+        }
+           lista = lista +
+                    `
+                   <tr>
+                        <td>${task.numberTask} </td>
+                        <td>${task.descriptionTask} </td>
+                        <td>${task.dateTask} </td>
+                        <td class="${status}">${task.statusTask} </td>
+                        
+                        <td>
+                        
+                            <img src="./acess/imagem/icones/editar.png" onclick ="editar(${task.id})">
+
+                            <img src="./acess/imagem/icones/excluir.png" onclick ="excluir(${task.id})">  
+                        
+                        </td>
+                   </tr>
+        
+            `
+        
+    });
+
+    document.getElementById('tbody').innerHTML = lista
+}
+
 
 let buscarTask = async () => {
     const response = await fetch("http://localhost:3000/atividades");
@@ -225,6 +636,7 @@ let carregarTask = async()=>{
     document.getElementById('tbody').innerHTML = lista
 }
 
+
 // ---------------------------------------------------------------
 
 // Dispara a ação para abrir o modal
@@ -254,23 +666,23 @@ cancModal.onclick = function () {
 
     // Verificar apos a conclusao das funcionalidades
 
-    // function cadastradoComSucesso() {
-    //     let sucesso = document.getElementById("alerta");
+    function cadastradoComSucesso() {
+        let sucesso = document.getElementById("alerta");
 
-    //     sucesso.innerHTML = 'Cadastrado com sucesso.';
+        sucesso.innerHTML = 'Cadastrado com sucesso.';
 
-    //     sucesso.classList.add("alert-success", "animate__fadeInUp"); //CSS do bootstrap
-    //     sucesso.classList.remove("d-none");
+        sucesso.classList.add("alert-success", "animate__fadeInUp"); //CSS do bootstrap
+        sucesso.classList.remove("d-none");
 
 
-    //     window.setTimeout(() => {
+        window.setTimeout(() => {
             
-    //         sucesso.classList.add("animate__fadeOutDown");
-    //         sucesso.classList.remove("d-none");
-    //     },
+            sucesso.classList.add("animate__fadeOutDown");
+            sucesso.classList.remove("d-none");
+        },
 
-    //         2000);
-    // }
+            2000);
+    }
 
     // function validadTask(){
     //     let erroTask = document.getElementById('alertaErro')
